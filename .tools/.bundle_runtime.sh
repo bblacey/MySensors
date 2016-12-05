556bd8c0ee49abba26f6a83063c2cca819d41cb6
Bruce B. Lacey <bruce@blacey.com>
2016-12-04 18:02:02 -0800

    Refactored tools structure & conventions for reuse
    1.  All common tools are hosted and managed in
        the tools directory (used for both local development
        and continuous integration)
    2.  Each tool comprises a directory, akin to a bundle,
        that encapsulates declarative command-line options,
        configuration files and a run script
    3.  A single bootstrap script configures a
        development environment
    4.  A lightweight runtime provides a common set of
        convenience functions and variables to all scripts

---
+++ .tools/.bundle_runtime.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+###
+#
+#  Tool runtime shim that provides a simple API for running
+#  tool bundles and convenience functions for other tool developers.
+#
+##
+log()  { IFS=" "; >&2 printf "%s\n" "$*";										}
+warn() { IFS=" "; >&2 printf "\e[33m%s\e[m\n" "$*";					}
+err()  { IFS=" "; >&2 printf '\e[31m%s\e[m\n' "$*"; exit 1;	}
+
+is_supported_os()
+{
+	[[ ${1} == darwin* ]] || [[ ${1} == linux-gnu ]] || [[ ${1} == freebsd ]] || [[ ${1} == msys ]] || [[ ${1} == cygwin ]]
+}
+
+is_installed()
+{
+	which "${1}" > /dev/null 2>&1
+}
+
+tools_dir()
+{
+	git_config_tools_dir='git config mysensors.toolsdir'
+
+	# Set mysensors.toolsdir in git config if it hasn't been set
+	if [ ! $($git_config_tools_dir) ]; then
+		$git_config_tools_dir "$( cd "$(dirname "${BASH_SOURCE[0]}")"; git rev-parse --show-prefix )"
+		git config alias.mystoolspath '![ -z "${GIT_PREFIX}" ] || cd ${GIT_PREFIX}; echo $(git rev-parse --show-cdup)$(git config mysensors.toolsdir)'
+	fi
+
+  $git_config_tools_dir	
+}
+
+modifiedSourceFiles()
+{
+	against=$(git rev-parse --verify HEAD >/dev/null 2>&1 && echo HEAD || echo 4b825dc642cb6eb9a060e54bf8d69288fbee4904)
+	git diff $1 --diff-filter=AM --name-only $against | grep -E '.*\.(c|cpp|h|hpp|ino)$'
+}
+
+stagedSourceFiles()
+{
+	modifiedSourceFiles '--cached'
+}
+
+runBundle() {
+	#local bundle="$(dirname "$(absolute_path "${0}")")"
+	local bundle="$(dirname "${0}")"
+	local tool=$(basename "${bundle}")
+
+	local  CMD_OPTIONS=$( TOOLCONFIG="${bundle}/config"	"${bundle}/options.sh" )
+
+	$tool $CMD_OPTIONS "$@"
+}
+
+###
+#  Common environment variables
+#
+$(git rev-parse --is-inside-work-tree --quiet >/dev/null 2>&1) || err "Working directory is not a git repository.  aborting..."
+
+GITREPO=$(git rev-parse --show-toplevel)
+GITDIR=$(git rev-parse --git-dir)
+TOOLSDIR="$(tools_dir)"
---
+++ .tools/bootstrap-dev.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+###
+#  OVERVIEW:
+#			This script bootstraps the MySensors development environment.
+#
+#  USAGE:
+#			git clone https://github.com/mysensors/MySensors.git
+#     cd MySensors
+#			.tools/bootstrap-dev.sh
+##
+
+##
+# Import common utility functions and environment variables
+. "$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)/.bundle_runtime.sh"
+
+check_tool_prerequisites() {
+	for preReq in ${1}; do
+		if ! is_installed $preReq; then
+			warn "$preReq not installed or not in current path.  Please install to continue configuring your environment and re-run $TOOLNAME"
+			return 1
+		fi
+	done
+}
+
+check_git_remote() {
+	local url=$( git config --get remote.${1}.url )
+	[[ $url == ${2} ]]
+}
+
+install_hooks() {
+	for hook in "${1}"/*.sh; do
+		local hookname=$(basename $hook)
+		$(cd "${GITDIR}/hooks"; ln -s -f "../../${TOOLSDIR}hooks/${hookname}" "${hookname%.sh}")
+	done
+}
+
+configure_git_tool_aliases() {
+	find "$(git mystoolspath)" -name run.sh -print0 | while IFS= read -r -d '' bundle; do
+		local tool="$(basename "$(dirname "${bundle}")")"
+		git config alias.${tool} '!f() { $(git mystoolspath)'${tool}'/run.sh $@; }; f'
+	done
+}
+
+###
+#  Main entry
+#
+#  1.  Check that we are bootstrapping a supported OS/environment
+#  2.  Validate github remotes include valid upstream and origin
+#  3.  Check for client commit hook pre-requisites
+#  4.  Install client commit hook pre-requisites
+#  5.  Define aliases for conveniently running tool bundles
+##
+
+mysrepo="https://github.com/mysensors/MySensors.git" 
+
+#1
+log "Checking operating system support: ${OSTYPE}..."
+is_supported_os ${OSTYPE} || {
+	err "OS ${OSTYPE} is unknown/unsupported, won't install anything"
+}
+
+#2
+log "Checking github 'origin' & 'upstream' remotes..."
+check_git_remote "origin" "${mysrepo}" && warn "Git \"origin\" should point to your github fork, not the github MySensors repo ${mysrep}"
+
+check_git_remote "upstream" "${mysrepo}" || {
+	warn "Git \"upstream\" remote not found or incorrectly defined. Configuring remote upstream --> ${mysrepo}..."
+	git remote add upstream "${mysrepo}" || err "git remote add ${mysrep} failed due to error $?"
+}
+
+#3
+log "Checking tool/utility pre-requisites..."
+check_tool_prerequisites "astyle cppcheck" || err "One or more required tools not found.  Complete installation of tool prequisites and re-run $TOOLNAME"
+
+#4
+log "Installing client-side git hooks..."
+install_hooks "$(git mystoolspath)hooks" || err "Failed to install git hooks due to error $?..."
+
+#5
+log "Configuring git aliases for running mysensor tools..."
+configure_git_tool_aliases || err "Failed to create git aliases due to error $?..."
+
+log "Your environment is configured for MySensors development... Thanks for your support!"
